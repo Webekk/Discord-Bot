0d6930c38749962c8f2db385e4ebb922bb2da399
Magikai <55356419+Webekk@users.noreply.github.com>
2021-11-03 20:55:03 +0100

    discord bot

---
+++ src/main/java/me/webek/bot/bot.java
@@ -0,0 +1,12 @@
+package me.webek.bot;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+public class bot {
+
+    public static void main(String[]args) throws Exception{
+        JDA api = JDABuilder.createDefault("OTAyMjY2OTU1MDUzMDM5NzA2.YXb7lQ.RZLUkGrDrqLgoIG7K1EqAKQwev8").build();
+
+    }
+}
