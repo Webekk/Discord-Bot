e16c0f4c39259095e60a2a188418f0536b600e2d
Webek <55356419+Webekk@users.noreply.github.com>
2021-11-07 13:58:07 +0100

    Update bot.java

--- src/main/java/me/webek/bot/bot.java
+++ src/main/java/me/webek/bot/bot.java
@@ -1,19 +1,27 @@
 package me.webek.bot;
 
+import Commands.Calculate;
+import Commands.InviteCommand;
+import Commands.UserInfoCommand;
+import Events.*;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
-public class bot {
+public class bot extends ListenerAdapter {
+
+
 
     public static void main(String[]args) throws Exception{
         JDA jda = JDABuilder.createDefault("TOKEN").build();
-        
+
         jda.addEventListener(new HelloEvent());
         jda.addEventListener(new CategoryCreate());
         jda.addEventListener(new CategoryDelete());
         jda.addEventListener(new VoiceChCreate());
         jda.addEventListener(new VoiceChDelete());
         jda.addEventListener(new Calculate());
-
+        jda.addEventListener(new InviteCommand());
+        jda.addEventListener(new UserInfoCommand());
     }
 }
