cfa7a816f322d4648ad9d6798a270adf7a896b0f
Webek <55356419+Webekk@users.noreply.github.com>
2021-11-09 22:12:17 +0100

    Update bot.java

--- src/main/java/me/webek/bot/bot.java
+++ src/main/java/me/webek/bot/bot.java
@@ -15,7 +15,7 @@ public class bot extends ListenerAdapter {
     public static void main(String[]args) throws Exception{
         JDA jda = JDABuilder.createDefault("TOKEN").build();
 
-        jda.addEventListener(new HelloEvent());
+       jda.addEventListener(new HelloEvent());
         jda.addEventListener(new CategoryCreate());
         jda.addEventListener(new CategoryDelete());
         jda.addEventListener(new VoiceChCreate());
@@ -23,5 +23,8 @@ public class bot extends ListenerAdapter {
         jda.addEventListener(new Calculate());
         jda.addEventListener(new InviteCommand());
         jda.addEventListener(new UserInfoCommand());
+        jda.addEventListener(new Filter());
+        jda.addEventListener(new FilterOnOff());
+        jda.addEventListener(new FilterMessage());
     }
 }
